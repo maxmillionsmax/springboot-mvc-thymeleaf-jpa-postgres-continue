d94e30af24a8ce4170a3375f6960356f94eecfaf
maxmillionsmax <maximilianocaetano@gmail.com>
2022-04-30 17:02:56 -0300

    Criando um filtro pelo nome das pessoas

--- src/main/java/curso/springboot/repository/PessoaRepository.java
+++ src/main/java/curso/springboot/repository/PessoaRepository.java
@@ -1,5 +1,8 @@
 package curso.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +13,7 @@ import curso.springboot.model.Pessoa;
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
 
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findPessoaByName(String nome);
+	
 }
