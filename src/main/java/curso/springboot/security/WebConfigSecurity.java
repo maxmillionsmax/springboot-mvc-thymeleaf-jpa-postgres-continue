60def4aa8ffce67874d6c2cc388f623e3831d7cb
maxmillionsmax <maximilianocaetano@gmail.com>
2022-05-03 16:52:22 -0300

    Spring Security-JPA-Banco de dados- validando Usuario

--- src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package curso.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,6 +15,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 	
 	@Override// Configura as solicitações de acesso por Http
 	protected void configure(HttpSecurity http) throws Exception {
@@ -30,9 +34,13 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
 	
 	@Override// Cria autenticação do usuario com banco de dados ou em memoria
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+		
+		/*  IN Memoria
 		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
 		.withUser("admin").password("$2a$10$gQ47DfNMbcyqzhxPXdyqKunmHRc9gk3wYxRzcfpw0DmP1rDWAvYa2").roles("ADMIN");
-		
+		*/
 	}
 	@Override
 	public void configure(WebSecurity web) throws Exception {
